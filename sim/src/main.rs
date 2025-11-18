7e17dd95e529c31a504fd1022a969f6a83e4558f
James Grisham <jgrisham4@gmail.com>
2025-11-18 14:11:58 -0600

    Working library deps.

--- sim/src/main.rs
+++ sim/src/main.rs
@@ -1,11 +1,11 @@
 use kinematics;
+use simcore::vec::Vec3;
 use std::f64::consts::PI;
-
 
 fn main() {
 
-    let v_a = kinematics::Vec3::new(1.0, 0.0, 0.0);
-    let axis = kinematics::Vec3::new(0.0, 0.0, 1.0).normalize();
+    let v_a = Vec3::new(1.0, 0.0, 0.0);
+    let axis = Vec3::new(0.0, 0.0, 1.0).normalize();
     let angle = 90.0 * PI / 180.0;
     let q_ba = kinematics::Quat::from_axis_angle(axis, angle);
     let v_b = q_ba.transform(v_a);
--- simcore/src/vec.rs
+++ simcore/src/vec.rs
@@ -1,96 +1,92 @@
-pub mod vec {
+/// Represents a vector of length 3.
+#[derive(Debug, Copy, Clone)]
+pub struct Vec3 {
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
+}
 
-    /// Represents a vector of length 3.
-    #[derive(Debug, Copy, Clone)]
-    pub struct Vec3 {
-        pub x: f64,
-        pub y: f64,
-        pub z: f64,
-    }
+impl Vec3 {
 
-    impl Vec3 {
-
-        /// Creates a new vector
-        ///
-        /// # Example
-        ///
-        /// ```
-        /// use simcore::vec::Vec3;
-        /// let v = Vec3::new(1.0, 0.2, 0.3);
-        /// ```
-        pub fn new(xv: f64, yv: f64, zv: f64) -> Vec3 {
-            Vec3 {
-                x: xv,
-                y: yv,
-                z: zv,
-            }
-        }
-
-        /// Dot product function
-        ///
-        /// # Example
-        ///
-        /// ```
-        /// use simcore::vec::Vec3;
-        /// let a = Vec3::new(1.0, 0.0, 0.0);
-        /// let b = Vec3::new(0.5, 0.5, 0.25);
-        /// let a_dot_b = a.dot(b);
-        ///
-        /// ```
-        pub fn dot(self, other: Vec3) -> f64 {
-            self.x * other.x + self.y * other.y + self.z * other.z
-        }
-
-        /// Computes cross product.
-        pub fn cross(self, other: Vec3) -> Vec3 {
-            Vec3 {
-                x: self.y * other.z - self.z * other.y,
-                y: self.z * other.x - self.x * other.z,
-                z: self.x * other.y - self.y * other.x,
-            }
-        }
-
-        /// Scales the present vector by the given factor.
-        pub fn scale(self, factor: f64) -> Vec3 {
-            Vec3 {
-                x: self.x * factor,
-                y: self.y * factor,
-                z: self.z * factor,
-            }
-        }
-
-        /// Element-wise addition of one vector with another.
-        pub fn add(self, other: Vec3) -> Vec3 {
-            Vec3 {
-                x: self.x + other.x,
-                y: self.y + other.y,
-                z: self.z + other.z,
-            }
-        }
-
-        /// Computes magnitude of vector.
-        pub fn mag(&self) -> f64 {
-            (self.x*self.x + self.y*self.y + self.z*self.z).sqrt()
-        }
-
-        /// Normalizes a vector.
-        pub fn normalize(&self) -> Vec3 {
-            let norm = self.mag();
-            Vec3 {
-                x: self.x / norm,
-                y: self.y / norm,
-                z: self.z / norm,
-            }
+    /// Creates a new vector
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use simcore::vec::Vec3;
+    /// let v = Vec3::new(1.0, 0.2, 0.3);
+    /// ```
+    pub fn new(xv: f64, yv: f64, zv: f64) -> Vec3 {
+        Vec3 {
+            x: xv,
+            y: yv,
+            z: zv,
         }
     }
 
+    /// Dot product function
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use simcore::vec::Vec3;
+    /// let a = Vec3::new(1.0, 0.0, 0.0);
+    /// let b = Vec3::new(0.5, 0.5, 0.25);
+    /// let a_dot_b = a.dot(b);
+    ///
+    /// ```
+    pub fn dot(self, other: Vec3) -> f64 {
+        self.x * other.x + self.y * other.y + self.z * other.z
+    }
+
+    /// Computes cross product.
+    pub fn cross(self, other: Vec3) -> Vec3 {
+        Vec3 {
+            x: self.y * other.z - self.z * other.y,
+            y: self.z * other.x - self.x * other.z,
+            z: self.x * other.y - self.y * other.x,
+        }
+    }
+
+    /// Scales the present vector by the given factor.
+    pub fn scale(self, factor: f64) -> Vec3 {
+        Vec3 {
+            x: self.x * factor,
+            y: self.y * factor,
+            z: self.z * factor,
+        }
+    }
+
+    /// Element-wise addition of one vector with another.
+    pub fn add(self, other: Vec3) -> Vec3 {
+        Vec3 {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+        }
+    }
+
+    /// Computes magnitude of vector.
+    pub fn mag(&self) -> f64 {
+        (self.x*self.x + self.y*self.y + self.z*self.z).sqrt()
+    }
+
+    /// Normalizes a vector.
+    pub fn normalize(&self) -> Vec3 {
+        let norm = self.mag();
+        Vec3 {
+            x: self.x / norm,
+            y: self.y / norm,
+            z: self.z / norm,
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
 
     use super::*;
-    use vec::Vec3;
+    use Vec3;
 
     #[test]
     fn can_instantiate_vec() {
